13781554bbde57b757db82103b80d52cc18133ec
Muhammad Usman Chughtai <20100993@mail.wit.ie>
2022-11-16 23:49:59 +0000

    Configured OpenApi

--- src/main/kotlin/ie/setu/controllers/UserController.kt
+++ src/main/kotlin/ie/setu/controllers/UserController.kt
@@ -5,14 +5,35 @@ import ie.setu.domain.repository.UserDAO
 import io.javalin.http.Context
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import io.javalin.plugin.openapi.annotations.*
 
 object UserController {
 
     private val userDao = UserDAO()
 
+
+    @OpenApi(
+        summary = "Get all users",
+        operationId = "getAllUsers",
+        tags = ["User"],
+        path = "/api/users",
+        method = HttpMethod.GET,
+        responses = [OpenApiResponse("200", [OpenApiContent(Array<User>::class)])]
+    )
+
     fun getAllUsers(ctx: Context) {
         ctx.json(userDao.getAll())
     }
+
+    @OpenApi(
+        summary = "Get user by ID",
+        operationId = "getUserById",
+        tags = ["User"],
+        path = "/api/users/{user-id}",
+        method = HttpMethod.GET,
+        pathParams = [OpenApiParam("user-id", Int::class, "The user ID")],
+        responses  = [OpenApiResponse("200", [OpenApiContent(User::class)])]
+    )
 
     fun getUserByUserId(ctx: Context) {
         val user = userDao.findById(ctx.pathParam("user-id").toInt())
@@ -21,12 +42,32 @@ object UserController {
         }
     }
 
+    @OpenApi(
+        summary = "Add User",
+        operationId = "addUser",
+        tags = ["User"],
+        path = "/api/users",
+        method = HttpMethod.POST,
+        pathParams = [OpenApiParam("user-id", Int::class, "The user ID")],
+        responses  = [OpenApiResponse("200")]
+    )
+
     fun addUser(ctx: Context) {
         val mapper = jacksonObjectMapper()
         val user = mapper.readValue<User>(ctx.body())
         userDao.save(user)
         ctx.json(user)
     }
+
+    @OpenApi(
+        summary = "Get user by Email",
+        operationId = "getUserByEmail",
+        tags = ["User"],
+        path = "/api/users/email/{email}",
+        method = HttpMethod.GET,
+        pathParams = [OpenApiParam("email", Int::class, "The user email")],
+        responses  = [OpenApiResponse("200", [OpenApiContent(User::class)])]
+    )
 
     fun getUserByEmail(ctx: Context) {
         val user = userDao.findByEmail(ctx.pathParam("email"))
@@ -35,9 +76,29 @@ object UserController {
         }
     }
 
+    @OpenApi(
+        summary = "Delete user by ID",
+        operationId = "deleteUserById",
+        tags = ["User"],
+        path = "/api/users/{user-id}",
+        method = HttpMethod.DELETE,
+        pathParams = [OpenApiParam("user-id", Int::class, "The user ID")],
+        responses  = [OpenApiResponse("204")]
+    )
+
     fun deleteUser(ctx: Context){
         userDao.delete(ctx.pathParam("user-id").toInt())
     }
+
+    @OpenApi(
+        summary = "Update user by ID",
+        operationId = "updateUserById",
+        tags = ["User"],
+        path = "/api/users/{user-id}",
+        method = HttpMethod.PATCH,
+        pathParams = [OpenApiParam("user-id", Int::class, "The user ID")],
+        responses  = [OpenApiResponse("204")]
+    )
 
     fun updateUser(ctx: Context){
         val mapper = jacksonObjectMapper()
