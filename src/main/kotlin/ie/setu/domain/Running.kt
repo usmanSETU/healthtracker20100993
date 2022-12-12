8211ea97f375fd01af485c086a882fc0ebd0c1f5
Muhammad Usman Chughtai <20100993@mail.wit.ie>
2022-12-13 02:38:49 +0500

    added Runnings Data layer

---
+++ src/main/kotlin/ie/setu/domain/Running.kt
@@ -0,0 +1,6 @@
+package ie.setu.domain
+
+data class Running(var id:Int,
+                   var distance:Int,
+                   var calories:Int,
+                   var userId:Int)
--- src/main/kotlin/ie/setu/utils/DBUtilities.kt
+++ src/main/kotlin/ie/setu/utils/DBUtilities.kt
@@ -3,8 +3,10 @@ package ie.setu.utils
 import ie.setu.domain.Activity
 import ie.setu.domain.User
 import ie.setu.domain.BloodPressure
+import ie.setu.domain.Running
 import ie.setu.domain.db.Activities
 import ie.setu.domain.db.BloodPressures
+import ie.setu.domain.db.Runings
 import ie.setu.domain.db.Users
 import org.jetbrains.exposed.sql.ResultRow
 
@@ -22,10 +24,17 @@ fun mapToActivities(it: ResultRow) = Activity(
     userId = it[Activities.userId]
 )
 
-fun mapToBloodPressure(it:ResultRow): BloodPressure = BloodPressure(
+fun mapToBloodPressure(it:ResultRow) = BloodPressure(
     id = it[BloodPressures.id],
     systolic = it[BloodPressures.systolic],
     diastolic = it[BloodPressures.diastolic],
     pulse = it[BloodPressures.pulse],
     userId = it[BloodPressures.userId]
+)
+
+fun mapToRuning(it: ResultRow)= Running(
+    id = it[Runings.id],
+    userId = it[Runings.userId],
+    calories = it[Runings.calories],
+    distance = it[Runings.distance]
 )
